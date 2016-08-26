e700c4a95b41b2d4cd6560d9347e28d526a9ba2a
djangobox <djangobox@163.com>
2016-08-26 23:30:22 +0800

    [view]:加入模板视图类
    Signed-off-by: djangobox <djangobox@163.com>

--- view/myapp/views.py
+++ view/myapp/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 
 # Create your views here.
 
-
+# base view example
 class MyView(View):
 
     def get(self, request, *args, **kwargs):
         return HttpResponse('Hello, World!')
+
+class MyTemplateView(TemplateView):
+    template_name = 'template.html'
+    myteststring = 'i am a variable from object:MyTemplateView'
+
+    def get_context_data(self, **kwargs):
+        context = super(MyTemplateView, self).get_context_data(**kwargs)
+        return  context
